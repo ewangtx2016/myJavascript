2bb8ef17778f835311fa96e24ba5c5a687a7ffb5
ewangtx2016 <18664698@qq.com>
2018-04-27 11:27:35 +0800

    新增了splitStr方法，可以将倒计时切割成单个字母拼段

--- time.js
+++ time.js
@@ -1,14 +1,21 @@
 /**
+ * ********
+ * 
  * FROM CHINA
  * name||email||wechart  : OLD`LV||18664698@qq.com||big_lv_
  * 
- * @{id} *必填 dom的id选择器 例如 ('DOM_NAME',
+ * 
+ * @{id} *必填 {string} dom的id选择器 例如 ('DOM_NAME',
+ * @{title} 选填 {string} 显示在倒计时前面的标题，默认为空
+ * @{splitTime} 选填 {bool} 是否将天、时、分、秒拆分成单个元素
+ * @{timeType} 选填 {number} 显示倒计时的样式 0-'**时**分**秒'  1-'**:**:**'
+ *  
  * 
  * let one = new TIME();
  * one.getTimeText(id,{
  *   startTime: '2018-04-26 21:49:00',  //* 可选 可以为时间戳
  *   endTime: '2018-04-26 23:33:00',    //* 可选 可以为时间戳
- *   title: 'title',                    //* 可选 
+ *   title: 'title',                    //* 可选  
  *   splitTime: true,                   //* 可选 
  *   timeType:0,                        //* 可选 
  *   timeCall:function(){               
@@ -16,6 +23,7 @@
  *   }
  * })
  * 
+ * ********
  */
 
  ;(function(window){
@@ -27,11 +35,7 @@
             this.id = id;
             this.obj = obj;
             this.tim = 0;
-            if(!obj.splitTime){
-                this.setTime()
-            }else{
-                this.setTimeSp()
-            }
+            this.setTime()
         },
         setTime: function(){
           this.cha = this.timeCha(); //object 类型 {is:false/true,string:''}
@@ -44,32 +48,55 @@
             // 计算d\h\m\s 的和
             let he = 0;
             let _this = this;
+            //默认标题
             let title = this.obj.title ? `<strong>${this.obj.title}</strong>` : '';
+            //默认样式
+            this.obj.timeType = this.obj.timeType ? this.obj.timeType : 0;
             for(let one of this.timeAr){
                 he += parseInt(one);
             }
-            if(this.obj.timeType == 0){
-                let dayTxt = this.timeAr[0] > 0 ? `<span>${this.timeAr[0]}</span><em>天</em>` : '';
-                txt =  `
-                    ${title}
-                    ${dayTxt}
-                    <span>${this.timeAr[1]}</span><em>时</em>
-                    <span>${this.timeAr[2]}</span><em>分</em>
-                    <span>${this.timeAr[3]}</span><em>秒</em>                
-                `;
-              }else{
-                let dayTxt = this.timeAr[0] > 0 ? `<span>${this.timeAr[0]}</span><em>:</em>` : '';
-                txt =  `
-                    ${title}
-                    ${dayTxt}
-                    <span>${this.timeAr[1]}</span><em>:</em>
-                    <span>${this.timeAr[2]}</span><em>:</em>
-                    <span>${this.timeAr[3]}</span>                
-                `;
-              }
-              console.log(this.timeAr)
-             
-              
+            if(!this.obj.splitTime){
+            	if(this.obj.timeType == 0){
+	                let dayTxt = this.timeAr[0] > 0 ? `<span>${this.timeAr[0]}</span><i>天</i>` : '';
+	                txt =  `
+	                    ${title}
+	                    ${dayTxt}
+	                    <span>${this.timeAr[1]}</span><i>时</i>
+	                    <span>${this.timeAr[2]}</span><i>分</i>
+	                    <span>${this.timeAr[3]}</span><i>秒</i>                
+	                `;
+	              }else{
+	                let dayTxt = this.timeAr[0] > 0 ? `<span>${this.timeAr[0]}</span><i>:</i>` : '';
+	                txt =  `
+	                    ${title}
+	                    ${dayTxt}
+	                    <span>${this.timeAr[1]}</span><i>:</i>
+	                    <span>${this.timeAr[2]}</span><i>:</i>
+	                    <span>${this.timeAr[3]}</span>                
+	                `;
+	              }
+            }else{
+            	let splitTimeAr = this.getTimeNewArr();
+            	if(this.obj.timeType == 0){
+	                let dayTxt = this.timeAr[0] > 0 ? `<span>${splitTimeAr[0][0]}</span><span>${splitTimeAr[0][1]}</span><i>天</i>` : '';
+	                txt =  `
+	                    ${title}
+	                    ${dayTxt}
+	                    <span>${splitTimeAr[1][0]}</span><span>${splitTimeAr[1][1]}</span><i>时</i>
+	                    <span>${splitTimeAr[2][0]}</span><span>${splitTimeAr[2][1]}</span><i>分</i>
+	                    <span>${splitTimeAr[3][0]}</span><span>${splitTimeAr[3][1]}</span><i>秒</i>                
+	                `;
+	              }else{
+	                let dayTxt = splitTimeAr[0] > 0 ? `<span>${splitTimeAr[0][0]}</span><span>${splitTimeAr[0][1]}</span><i>:</i>` : '';
+	                txt =  `
+	                    ${title}
+	                    ${dayTxt}
+	                    <span>${splitTimeAr[1][0]}</span><span>${splitTimeAr[1][1]}</span><i>:</i>
+	                    <span>${splitTimeAr[2][0]}</span><span>${splitTimeAr[2][1]}</span><i>:</i>
+	                    <span>${splitTimeAr[3][0]}</span><span>${splitTimeAr[3][1]}</span>               
+	                `;
+	              }
+            }
 
             if(he == 0){
                 // 添加dom
@@ -88,8 +115,14 @@
           document.querySelector('#'+this.id).innerHTML = txt;
           
         },
-        setTimeSp: function(){
-            
+        getTimeNewArr: function(){
+        	// 把d,h,m,s拆分成数组
+        	let newArr = [];
+            for(let one of this.timeAr){
+            	one += '';
+            	newArr.push(one.split(''));
+            }
+            return newArr;
         },
         getDMS: function(timeNub){
             let timeNubNow = timeNub;
